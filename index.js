25cbcf4ed9f342202eca2799e2fb4968b9e607f2
Patrick Modafferi <pat.modafferi@gmail.com>
2017-11-15 17:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function switchOnCharmFromTip(amount){
   if (amount = 'generous'){
     return 'Thank you so much.'
   }
-  if (amount == 'not as generous') {
+  if else (amount == 'not as generous') {
     return 'Thank you.'
   }
 }
